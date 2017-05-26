83719230ce8f6934f97ea1503aad52882ec96977
Jake Papaalia <Jpapalia@jakes-macbook-air-5.local>
2017-05-25 21:53:31 -0500

    GitHub Setup

---
+++ TicTacToe/GridLabel.swift
@@ -0,0 +1,14 @@
+//
+//  GridLabel.swift
+//  TicTacToe
+//
+//  Created by Jake Papaalia on 11/15/16.
+//  Copyright © 2016 Jake Papalia. All rights reserved.
+//
+
+import UIKit
+
+class GridLabel: UILabel {
+    var canTap = true
+
+}
